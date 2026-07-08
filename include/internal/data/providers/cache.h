689d2d4d9e93321ec4a798f6ca499aa6b91fb7f5
Róbert Čerňanský <openhs@tightmail.com>
2026-07-08 19:51:14 +0200

    Fix build failure with GCC 17.

--- include/internal/data/providers/cache.h
+++ include/internal/data/providers/cache.h
@@ -3,7 +3,7 @@
 // Project: Ampache Browser
 // License: GNU GPLv3
 //
-// Copyright (C) 2015 - 2023 Róbert Čerňanský
+// Copyright (C) 2015 - 2026 Róbert Čerňanský
 
 
 
@@ -16,6 +16,7 @@
 #include <map>
 #include <memory>
 #include <chrono>
+#include <fstream>
 #include <QObject>
 #include <QPixmap>
 
